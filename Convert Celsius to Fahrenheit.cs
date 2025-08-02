35a28a71976637f5813859b6d3139cc073649d44
Harshada Raut <136487511+Harshada-77@users.noreply.github.com>
2025-08-02 12:44:46 +0530

    Create Convert Celsius to Fahrenheit.cs

---
+++ Convert Celsius to Fahrenheit.cs
@@ -0,0 +1,10 @@
+using System;
+class TempConversion {
+    static void Main() {
+        Console.Write("Enter temperature in Celsius: ");
+        double celsius = double.Parse(Console.ReadLine());
+
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        Console.WriteLine($"Fahrenheit: {fahrenheit}");
+    }
+}
